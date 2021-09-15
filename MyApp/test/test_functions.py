44240dc861da0e556ff78cac51313f4a8b5c5168
snufka <52801250+snufka@users.noreply.github.com>
2021-09-15 22:26:08 +0200

    cleaning up2

--- MyApp/test/test_functions.py
+++ MyApp/test/test_functions.py
@@ -1,16 +1,5 @@
 import pytest
 from ..text_lan_url import url_lang
-#url = ''
-# def lang_in_url(url):
-#split = url.split(".")
-
-# Checking Language by subDomain;
-# if split[0].find("en") == -1:
-#print("*********Based on the URL, this page is in German**************")
-# elif (split[0].includes("en"))
-#     print("it's not english nor German, to find out more please visit https://en.wikipedia.org/wiki/List_of_Wikipedias#List")
-# else:
-#  print("*********Based on the URL, this page is in English*****************")
 
 
 def test_for_url_land():
--- test_functions.py
+++
@@ -1,6 +0,0 @@
-#Testing
-import pytest
-
-def test_for_url_land():
-  assert lang_in_url('https://de.wikipedia.org/wiki/Angela_Merkel')==('*********Based on the URL, this page is in German**************')
-  
